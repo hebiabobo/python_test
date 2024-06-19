33e4eaf210565014a7d83da005fe63c16a3c75a6
hebiabobo <3292705660@qq.com>
2024-06-19 11:58:02 +0900

    Changes

--- basetest_python/sort_test.py
+++ basetest_python/sort_test.py
@@ -27,5 +27,21 @@ def bubble_sort(alist):
         print(alist)
 
 
+def quicksort(alist):
+    if len(alist) <= 1:
+        return alist
+    else:
+        pivot = alist[len(alist) // 2]  # 选择中间元素作为基准
+        left = [x for x in alist if x < pivot]  # 小于基准的元素
+        middle = [x for x in alist if x == pivot]  # 等于基准的元素
+        right = [x for x in alist if x > pivot]  # 大于基准的元素
+        return quicksort(left) + middle + quicksort(right)
+
+
+# 示例
+arr = [3, 6, 8, 10, 1, 2, 1]
+sorted_arr = quicksort(arr)
+print(sorted_arr)
+
 # selection_sort(a)
-bubble_sort(a)
+# bubble_sort(a)
