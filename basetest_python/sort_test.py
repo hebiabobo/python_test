868a40ea53416b82c61d8266d2162770361800ac
hebiabobo <3292705660@qq.com>
2024-06-23 17:33:26 +0900

    Changes

--- basetest_python/sort_test.py
+++ basetest_python/sort_test.py
@@ -38,9 +38,62 @@ def quicksort(alist):
         return quicksort(left) + middle + quicksort(right)
 
 
-# 示例
-arr = [3, 6, 8, 10, 1, 2, 1]
-sorted_arr = quicksort(arr)
+def shell_sort(alist):
+    n = len(alist)
+    gap = n // 2
+    while gap:
+        for i in range(gap, n):  # 每一个gap里面的元素比较
+            while i - gap >= 0 and alist[i - gap] > alist[i]:
+                alist[i - gap], alist[i] = alist[i], alist[i - gap]
+                print("i:", i)
+                i -= gap
+                print("i = i - gap:", i)
+                print(alist)
+            print("222")
+        gap //= 2
+        print("333")
+    return alist
+
+
+def heapify(arr, n, i):
+    largest = i  # 初始化最大值为根节点
+    left = 2 * i + 1  # 左子节点
+    right = 2 * i + 2  # 右子节点
+
+    # 如果左子节点存在且大于根节点
+    if left < n and arr[i] < arr[left]:
+        largest = left
+
+    # 如果右子节点存在且大于最大值
+    if right < n and arr[largest] < arr[right]:
+        largest = right
+
+    # 如果最大值不是根节点
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]  # 交换
+        heapify(arr, n, largest)  # 递归堆化子树
+
+
+def heap_sort(arr):
+    n = len(arr)
+
+    # 构建最大堆
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    # 一个个交换元素
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]  # 交换
+        heapify(arr, i, 0)  # 堆化根节点
+
+    return arr
+
+
+arr = [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+# arr = [3, 6, 8, 10, 1, 2, 1, 15, 4, 7, 9, 11]
+# sorted_arr = quicksort(arr)
+# sorted_arr = shell_sort(arr)
+sorted_arr = heap_sort(arr)
 print(sorted_arr)
 
 # selection_sort(a)
